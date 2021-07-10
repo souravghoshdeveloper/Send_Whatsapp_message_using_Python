16dcd06f023345d6600ec20f4cc391c0d77326fb
Sourav Ghosh <infosourav04@gmail.com>
2021-07-10 09:36:01 +0530

    Initial commit

---
+++ Send msg to group.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg_to_group("xxxxxxxxxxxxxxxxxxxxx", "Hi, this is a system generated msg", 11, 28)
---
+++ Sourav.py
@@ -0,0 +1,2 @@
+import pywhatkit as pw
+pw.sendwhatmsg("+91xxxxxxxxxx", "Hi, This is a System Generated Msg.", 11, 32)
